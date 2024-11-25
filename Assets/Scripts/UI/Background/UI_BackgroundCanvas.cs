d761cc5140f83eec31dc32a5c23fbb93827e47d2
DaeHan Jang <abouttt57@gmail.com>
2024-11-25 19:00:21 +0900

    아이템 버리기 기능 추가

---
+++ Assets/Scripts/UI/Background/UI_BackgroundCanvas.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_BackgroundCanvas : UI_View, IPointerDownHandler, IDropHandler
+{
+    protected override void Init()
+    {
+        UIManager.Register(this);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        InputManager.CursorLocked = true;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag.TryGetComponent<UI_ItemSlot>(out var itemSlot))
+        {
+            OnDropItemSlot(itemSlot);
+        }
+    }
+
+    private void OnDropItemSlot(UI_ItemSlot itemSlot)
+    {
+        var itemInventory = UIManager.Get<UI_ItemInventoryPopup>().ItemInventory;
+        var item = itemInventory.GetItem<Item>(itemSlot.Index);
+        string guideText = $"[{item.Data.ItemName}] {GuideSettings.Instance.DestroyText}";
+        UIManager.Show<UI_ConfirmationPopup>().SetEvent(() =>
+        {
+            itemInventory.RemoveItem(itemSlot.Index);
+        },
+        guideText);
+    }
+}
