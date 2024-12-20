e210ccc9d7276e68c8354e944088c0d2b9da195d
DaeHan Jang <abouttt57@gmail.com>
2024-12-20 18:53:14 +0900

    UISettingsEditor 추가

---
+++ Assets/Scripts/Editor/UISettingsEditor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UISettings), true)]
+public class UISettingsEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        DrawDefaultInspector();
+
+        var settings = (UISettings)target;
+
+        if (GUILayout.Button("Refresh UI Prefabs")) 
+        { 
+            settings.RefreshUIPrefabs();
+        }
+    }
+}
