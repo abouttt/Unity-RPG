0dbf6c83cbe0f413db061409509fe4eb15473aa9
DaeHan Jang <abouttt57@gmail.com>
2025-02-20 19:01:49 +0900

    락온 UI 추가

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -5,6 +5,8 @@ public class GameScene : BaseScene
     protected override void Init()
     {
         base.Init();
+        UIManager.InitUISettings();
+        ConnectUI();
     }
 
     private void Start()
@@ -12,5 +14,12 @@ public class GameScene : BaseScene
         InputManager.Enabled = true;
         InputManager.CursorLocked = true;
         SoundManager.Play2D(SceneSettings.Instance[SceneLoader.CurrentSceneName].BGM, SoundType.BGM);
+    }
+
+    private void ConnectUI()
+    {
+        var player = GameObject.FindWithTag("Player");
+        var lockOnFov = Camera.main.GetComponent<FieldOfView>();
+        UIManager.Get<UI_LockOn>().Connect(lockOnFov);
     }
 }
---
+++ Assets/Scripts/UI/Auto/UI_LockOn.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class UI_LockOn : UI_View, IConnectable<FieldOfView>
+{
+    public FieldOfView Context => _lockOnFovRef;
+
+    private FieldOfView _lockOnFovRef;
+
+    protected override void Init()
+    {
+        base.Init();
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        Disconnect();
+    }
+
+    public void Connect(FieldOfView lockOnFov)
+    {
+        Disconnect();
+
+        _lockOnFovRef = lockOnFov;
+        lockOnFov.TargetChanged += SetTarget;
+    }
+
+    public void Disconnect()
+    {
+        if (_lockOnFovRef != null)
+        {
+            _lockOnFovRef.TargetChanged -= SetTarget;
+            _lockOnFovRef = null;
+        }
+    }
+
+    private void SetTarget(Transform target)
+    {
+        Get<UI_FollowWorldObject>("LockOn").Target = target;
+        gameObject.SetActive(target != null);
+    }
+}
