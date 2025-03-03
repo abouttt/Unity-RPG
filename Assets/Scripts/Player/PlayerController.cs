244f9b507bbfe7dc53865fa9c089a36096d5a799
DaeHan Jang <abouttt57@gmail.com>
2025-03-03 14:59:05 +0900

    PlayerController 추가

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _runSpeed;
+
+    [SerializeField]
+    private float _sprintSpeed;
+
+    [SerializeField]
+    private float _inAirSpeed;
+
+    [SerializeField]
+    private float _landingSpeed;
+
+    [SerializeField]
+    private float _jumpForce;
+
+    private bool _isJumped;
+    private bool _isJumpedWithInput;
+
+    // Input Value
+    private Vector2 _move;
+    private bool _isPressedSprint;
+    private bool _isPressedJump;
+
+    private GameObject _mainCamera;
+    private GroundedCharacterController _movement;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main.gameObject;
+        _movement = GetComponent<GroundedCharacterController>();
+    }
+
+    private void Update()
+    {
+        CheckJump();
+        UpdateMoveSpeed();
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        var inputDirection = new Vector3(_move.x, 0f, _move.y);
+
+        if (inputDirection != Vector3.zero)
+        {
+            float cameraYaw = _mainCamera.transform.eulerAngles.y;
+            float yaw = GetYaw(inputDirection) + cameraYaw;
+            var movementDirection = Quaternion.Euler(0f, yaw, 0f) * Vector3.forward;
+
+            _movement.Move(movementDirection);
+        }
+        else
+        {
+            if (!_isJumped)
+            {
+                _movement.Move(Vector3.zero);
+            }
+        }
+    }
+
+    private void Rotate()
+    {
+        var inputDirection = new Vector3(_move.x, 0f, _move.y);
+
+        if (inputDirection != Vector3.zero)
+        {
+            float cameraYaw = _mainCamera.transform.eulerAngles.y;
+            float yaw = GetYaw(inputDirection) + cameraYaw;
+            var rotationDirection = new Vector3(0f, yaw, 0f);
+
+            _movement.Rotate(rotationDirection);
+        }
+    }
+
+    private void CheckJump()
+    {
+        if (_isJumped)
+        {
+            if (_movement.IsGrounded &&
+                !_movement.IsJumping &&
+                !_movement.IsFalling)
+            {
+                _isJumped = false;
+            }
+
+            _isPressedJump = false;
+        }
+        else
+        {
+            if (_isPressedJump)
+            {
+                _isJumped = true;
+                _isJumpedWithInput = _move != Vector2.zero;
+                _isPressedJump = false;
+                _movement.Jump(_jumpForce);
+            }
+        }
+    }
+
+    private void UpdateMoveSpeed()
+    {
+        if (_movement.IsGrounded)
+        {
+            _movement.MoveSpeed = _movement.IsLanding ? _landingSpeed
+                                : _isPressedSprint ? _sprintSpeed
+                                : _runSpeed;
+        }
+        else if (_isJumped)
+        {
+            if (!_isJumpedWithInput)
+            {
+                _movement.MoveSpeed = _inAirSpeed;
+            }
+        }
+        else if (_movement.IsFalling)
+        {
+            _movement.MoveSpeed = _inAirSpeed;
+        }
+    }
+
+    private float GetYaw(Vector3 direction)
+    {
+        return Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+    }
+
+    // Input System Callbacks
+
+    public void OnMove(InputValue inputValue)
+    {
+        _move = inputValue.Get<Vector2>();
+    }
+
+    public void OnSprint(InputValue inputValue)
+    {
+        _isPressedSprint = inputValue.isPressed;
+    }
+
+    public void OnJump(InputValue inputValue)
+    {
+        _isPressedJump = inputValue.isPressed;
+    }
+}
