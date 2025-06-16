1982b8652437e4bd2a79ad18384a9f080efe4658
DaeHan Jang <abouttt57@gmail.com>
2025-06-17 03:28:48 +0900

    필드 아이템 추가

---
+++ Assets/Scripts/Interaction/FieldItem.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using AYellowpaper.SerializedCollections;
+
+public class FieldItem : Interactable
+{
+    public IReadOnlyDictionary<ItemData, int> Items => _items;
+
+    [SerializeField]
+    private bool _destroyWhenEmpty = true;
+
+    [SerializeField, SerializedDictionary("Item", "Quantity")]
+    private SerializedDictionary<ItemData, int> _items;
+
+    private void Start()
+    {
+        if ((_items == null || _items.Count == 0) && _destroyWhenEmpty)
+        {
+            Managers.Resource.Destroy(gameObject);
+        }
+    }
+
+    public void AddItem(ItemData itemData, int quantity)
+    {
+        if (quantity <= 0)
+        {
+            return;
+        }
+
+        if (!_items.ContainsKey(itemData))
+        {
+            _items.Add(itemData, 0);
+        }
+
+        _items[itemData] += quantity;
+    }
+
+    public void RemoveItem(ItemData itemData, int quantity)
+    {
+        if (quantity <= 0)
+        {
+            return;
+        }
+
+        if (_items.ContainsKey(itemData))
+        {
+            _items[itemData] -= quantity;
+
+            if (_items[itemData] <= 0)
+            {
+                _items.Remove(itemData);
+            }
+        }
+
+        if (_items.Count == 0 && _destroyWhenEmpty)
+        {
+            Managers.Resource.Destroy(gameObject);
+        }
+    }
+
+    public override void OnDetected(Interactor interactor)
+    {
+
+    }
+
+    public override void OnInteractionEnded(Interactor interactor)
+    {
+
+    }
+
+    public override void OnInteractionStarted(Interactor interactor)
+    {
+
+    }
+
+    public override void OnUndetected(Interactor interactor)
+    {
+
+    }
+}
